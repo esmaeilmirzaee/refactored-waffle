860a7a33d63d4267a5dcf6710349acf64fe08e42
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-07-06 20:44:43 +0430

    Input enabled

--- timer/options.js
+++ timer/options.js
@@ -1,4 +1,10 @@
-chrome.storage.local.get(['timer'], (res) => {
+chrome.storage.sync.get(['timer', 'delay'], (res) => {
     console.log(res.timer);
     document.getElementById('options_time').innerText = res.timer ?? 0;
+    document.getElementById('delay').value = res.delay ?? 1000;
+});
+
+chrome.storage.sync.set({
+    timer: new Date().toLocaleTimeString(),
+    delay: document.getElementById('delay').value,
 });
